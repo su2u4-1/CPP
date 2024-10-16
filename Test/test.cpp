8bac9ed4b6bfd4bb87f8ca52d03918f9099bc8ff
su2u4 <joey23.su@gmail.com>
2024-10-16 14:42:59 +0800

    .

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    string s;
+    string m = " `1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+    while (cin >> s) {
+        for (int i = 0; i < s.length(); i++) {
+        }
+    }
     return 0;
 }
