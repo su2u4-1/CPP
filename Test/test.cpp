36c0607bb6b01e5a3bbc11d690536442e306e178
su2u4 <Joey23.su@gmail.com>
2025-05-26 17:55:33 +0800

    .

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,9 +1,15 @@
+#include <bitset>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
-
+    string a = "123";
+    string b = "12345678901234567890123456789012345678901234567890";
+    cout << sizeof(a) << ", " << &a << ", " << static_cast<const void*>(a.data()) << endl
+         << sizeof(b) << ", " << &b << ", " << static_cast<const void*>(b.data()) << endl;
+    for (long long i = 0x7ffffcba0; i < 0x7ffffcc00; i++) {
+        cout << hex << i << dec << ": " << bitset<8>(*reinterpret_cast<char*>(i)) << endl;
+    }
     return 0;
 }
