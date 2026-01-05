2273efe202e02700fbed6a312d5f3478076ee6e9
su2u4 <Joey23.su@gmail.com>
2026-01-05 13:55:02 +0800

    set cpp format setting and clangd setting

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,15 +1,7 @@
-#include <bitset>
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    string a = "123";
-    string b = "12345678901234567890123456789012345678901234567890";
-    cout << sizeof(a) << ", " << &a << ", " << static_cast<const void*>(a.data()) << endl
-         << sizeof(b) << ", " << &b << ", " << static_cast<const void*>(b.data()) << endl;
-    for (long long i = 0x7ffffcba0; i < 0x7ffffcc00; i++) {
-        cout << hex << i << dec << ": " << bitset<8>(*reinterpret_cast<char*>(i)) << endl;
-    }
+    cout << "Hello, World!" << endl;
     return 0;
 }
